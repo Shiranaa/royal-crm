83db1baa92fb9c7f5f629f37a549fadafbacea65
shiraaanm@gmail.com <shiraaanm@gmail.com>
2022-05-08 20:56:13 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const customersModule = require('../controllers/customers');
-const productsModule = require('../controllers/products');
+const customersModule = require("../controllers/customers");
+const productsModule = require("../controllers/products");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.send('hello again');
-router.get('/', function (req, res, next) {
-  customersModule.addCustomer('Lola', '05233333', 'hi@gmail.com', 1);
+router.get("/", function (req, res, next) {
+  customersModule.addCustomer("Lola", "05233333", "hi@gmail.com", 1);
   customersModule.customersList(req, res);
 });
 
-router.get('/products', function (req, res, next) {
-  productsModule.addProduct('Good Product', 'A very good product', 50);
+router.get("/products", function (req, res, next) {
+  productsModule.addProduct("Good Product", "A very good product", 50);
   productsModule.productsList(req, res);
 });
 
