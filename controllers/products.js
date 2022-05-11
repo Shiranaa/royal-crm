7aa5ab5a7eedc7e42ffe1ec1a91301526019187e
shiraaanm@gmail.com <shiraaanm@gmail.com>
2022-05-11 18:55:21 +0300

    mysql2 promise

--- controllers/products.js
+++ controllers/products.js
@@ -1,51 +1,37 @@
-const mysql = require("mysql2");
-const config = require("../config/dev");
-
-const pool = mysql.createPool({
-  host: config.DB_HOST,
-  user: config.DB_USER,
-  password: config.DB_PASSWORD,
-  database: config.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 5,
-  queueLimit: 0,
-});
+const database = require("./database");
 
 module.exports = {
-  addProduct: function (name, desc, price) {
-    if (!name || name.length === 0) {
-      throw "ERROR: name is empty";
+  // addProduct: function (name, desc, price) {
+  //     if (!name || name.length === 0) {
+  //         throw ('ERROR: name is empty');
+  //     }
+
+  //     database.pool.getConnection(function (connErr, connection) {
+  //         if (connErr) throw connErr; // not connected!
+
+  //         const sql = "INSERT INTO products(name, description, price)" +
+  //             " VALUES(?,?,?);";
+
+  //         connection.query(
+  //             sql,
+  //             [name, desc, price],
+  //             function (sqlErr, result, fields) {
+  //                 if (sqlErr) throw sqlErr;
+
+  //                 console.log(result);
+  //             });
+  //     });
+  // },
+
+  productsList: async function (req, res, next) {
+    const sql = "SELECT * FROM products";
+
+    try {
+      const connection = await database.getConnection();
+      const result = await database.runQuery(connection, sql);
+      res.send(result);
+    } catch (err) {
+      console.log(err);
     }
-
-    pool.getConnection(function (connErr, connection) {
-      if (connErr) throw connErr; // not connected!
-
-      const sql =
-        "INSERT INTO products(name, description, price)" + " VALUES(?,?,?);";
-
-      connection.query(
-        sql,
-        [name, desc, price],
-        function (sqlErr, result, fields) {
-          if (sqlErr) throw sqlErr;
-
-          console.log(result);
-        }
-      );
-    });
-  },
-
-  productsList: function (req, res) {
-    pool.getConnection(function (connErr, connection) {
-      if (connErr) throw connErr; // not connected!
-
-      const sql = "SELECT * FROM products";
-
-      connection.query(sql, function (sqlErr, result, fields) {
-        if (sqlErr) throw sqlErr;
-
-        res.send(result);
-      });
-    });
   },
 };
