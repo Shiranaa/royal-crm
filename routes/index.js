5e349eb883e8819ce887035ab8358a037d43d29c
shiraaanm@gmail.com <shiraaanm@gmail.com>
2022-05-11 21:26:24 +0300

    request parsing

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const customersModule = require("../controllers/customers");
+const cm = require("../controllers/customers");
 const productsModule = require("../controllers/products");
+const ordersModule = require("../controllers/orders");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  customersModule.addCustomer("Lola", "05233333", "hi@gmail.com", 1);
-  customersModule.customersList(req, res);
+  res.send("this is the home page. use /customers /products or /orders.");
 });
 
-router.get("/products", function (req, res, next) {
-  productsModule.addProduct("Good Product", "A very good product", 50);
-  productsModule.productsList(req, res);
-});
+/* customers */
+router.get("/customers", cm.customersList);
+router.get("/customers-add", cm.addCustomer);
+
+// todo: delete customer
+router.delete("/customers", cm.deleteCustomer);
+
+// todo: export all customers to file
+router.get("/customers-export", cm.exportCustomers);
+
+// todo: edit/update customer
+router.patch("/customers", cm.updateCustomer);
+
+// todo: view more details of a customer
+router.get("/customer-detailes", cm.viewCustomerDetails);
+
+/* products */
+router.get("/products", productsModule.productsList);
+
+/* orders */
+router.get("/orders", ordersModule.ordersList);
 
 module.exports = router;
