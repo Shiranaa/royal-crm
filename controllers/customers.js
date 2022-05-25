28dbbe0857a28a610c4a099feb394928f0257e9e
shiraaanm@gmail.com <shiraaanm@gmail.com>
2022-05-25 19:24:03 +0300

    auth

--- controllers/customers.js
+++ controllers/customers.js
@@ -13,14 +13,17 @@ module.exports = {
         .string()
         .required()
         .regex(/^[0-9]\d{8,11}$/),
-      email: joi.string().required(),
+      email: joi
+        .string()
+        .required()
+        .regex(/^[^@]+@[^@]+$/),
       countryInputHtml: joi.number().required(),
     });
 
     const { error, value } = schema.validate(reqBody);
 
     if (error) {
-      res.send(`error adding customer: ${error}`);
+      `error adding customer: ${error}`;
       return;
     }
 
