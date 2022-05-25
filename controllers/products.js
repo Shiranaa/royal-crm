bab3c15d328880ce8b5544890cada8a7acff563d
shiraaanm@gmail.com <shiraaanm@gmail.com>
2022-05-25 19:23:51 +0300

    auth

--- controllers/products.js
+++ controllers/products.js
@@ -81,7 +81,7 @@ module.exports = {
 
   // todo: delete product
   deleteProduct: async function (req, res, next) {
-    // const sql = DROP
+    // const sql = DELETE
     res.send("todo delete products");
   },
 
